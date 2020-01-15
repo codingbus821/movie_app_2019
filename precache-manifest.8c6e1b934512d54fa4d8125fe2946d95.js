af2477098f23c2eef0db239d1d559f5781dc1a27
kimbyeongsoo <kbsu147@naver.com>
2020-01-15 20:29:52 +0900

    Updates

---
+++ precache-manifest.8c6e1b934512d54fa4d8125fe2946d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c236a0b07e1c2ce34a13b17cb18ecc5",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "243fc140dbcb7cd6374b",
+    "url": "/movie_app_2019/static/css/main.8adfbc7f.chunk.css"
+  },
+  {
+    "revision": "7ceb3f025d671189f167",
+    "url": "/movie_app_2019/static/js/2.db40e205.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.db40e205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "243fc140dbcb7cd6374b",
+    "url": "/movie_app_2019/static/js/main.f457a62c.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
